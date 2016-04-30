a4cf03ebe6562e73731d3f67315793e726404a64
Jake Kalman <jacob.kalman@fmr.com>
2016-04-30 17:24:39 -0400

    set up css minification; replaced jshint with eslint to be able to use es6; reordered grunt to minify es5 compliant file instead of es6 which is currently not supported by uglify; cleaned up gruntfile so sass/css and js are managed one after another

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,36 +3,62 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    // compile sass into css
+    sass: {
+      dist: {
+        files: {
+          'dist/css/styles.css': 'sass/styles.scss'
+        }
+      }
+    },
+    // minify css
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'dist/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    // lint the es6 code
+    eslint: {
+		    target: ['src/main.js']
+    },
+    // convert es6 code to es5
+    babel: {
+      options: {
+        presets: ['es2015']
+      },
+      dist: {
+        files: {
+          'dist/js/main.js': 'src/main.js'
+        }
+      }
+    },
+    // minify the es5 compliant code
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'js/main.js',
-        dest: 'build/js/main.min.js'
+        src: 'dist/js/main.js',
+        dest: 'dist/js/main.min.js'
       }
-    },
-    sass: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        files: {
-          'build/css/styles.css': 'sass/styles.scss'
-        }
-      }
-    },
-    jshint: {
-      all: ['Gruntfile.js', 'js/**/*.js', 'test/**/*.js']
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load plugins
   grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  // Default task(s).
-  grunt.registerTask('default', ['uglify', 'sass', 'jshint']);
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Default task(s)
+  grunt.registerTask('default', ['sass', 'cssmin', 'eslint', 'babel', 'uglify']);
 
 
 
