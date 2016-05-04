90bc503b1b2e0e1374c82141e25053535d9a1e71
Jake Kalman <jacob.kalman@fmr.com>
2016-05-04 14:39:18 -0400

    changed application to messaging, reformatted html, added fontawesome, added moment js for timestamp, added data changed event subscription

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 var $ = require('jquery');
 var Vue = require('vue');
 var Firebase = require('firebase');
-
+var moment = require('moment');
 // explicit installation required in module environments
 Vue.use(require('vuefire'));
 Vue.use(require('vue-resource'));
@@ -10,63 +10,64 @@ Vue.use(require('vue-resource'));
 /* EVENTS VUE */
 
 new Vue({
-  // target events div
-  el: '#events',
+  // target messages div
+  el: '#messages',
   // set up firebase connection
   firebase: {
-    events: new Firebase('https://radiant-torch-6650.firebaseio.com/events')
+    messages: new Firebase('https://radiant-torch-6650.firebaseio.com/messages').limitToLast(4)
   },
   // anything within the ready function will run when
   // the application loads, call methods to initialize the app
   // with data
   ready: function() {
     // when the application loads, call the initialize data method
-    //this.fetchEvents();
-    // var events = this.$firebaseRefs.eventsDataRef;
+    //this.fetchMessages();
+    // var messages = this.$firebaseRefs.messagesDataRef;
     this.subscribe();
-    //this.$set('events', eventData);
+    //this.$set('messages', messageData);
   },
 
   // initial object
   data: {
-    event: { name: '', description: '', date: '' }
+    message: { name: '', description: '', date: '' }
   },
 
   // custom methods registered here
   methods: {
     subscribe: function() {
-      // var events = this.events;
-      //
-      // events.on("value", function(snapshot) {
-      //   console.log(snapshot.val());
-      // }, function (errorObject) {
-      //   console.log("The read failed: " + errorObject.code);
-      // });
+      var messages = this.$firebaseRefs.messages;
+
+      messages.on("value", function(snapshot) {
+        console.log(snapshot.val());
+      }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
     },
     // method to retrieve and set data
-    fetchEvents: function() {
+    fetchMessages: function() {
 
 
     },
 
-    // adds an event to the existing events array
-    addEvent: function() {
-      if (this.event.name) {
-        var events = this.$firebaseRefs.events;
-        // add event
-        events.push(this.event);
-        // reset event
-        this.event = {name: '', description: '', date: ''};
-      }
-    },
-
-    // deletes an event via the passed in index value
-    deleteEvent: function(event) {
-      if (confirm("Are you sure you want to delete this event?")) {
-        // this is going to change in 2.0, so I may need to rewrite this using
-        // $index and splice
-        this.events.$remove(event);
+    // adds an message to the existing messages array
+    addMessage: function() {
+      if (this.message.name) {
+        this.message.date = moment().format('YYYY-MM-DD HH:mm:ss');
+        var messages = this.$firebaseRefs.messages;
+        // add message
+        messages.push(this.message);
+        // reset message
+        this.message = {description: '', date: ''};
       }
     }
+    //
+    // // deletes an message via the passed in index value
+    // deleteMessage: function(message) {
+    //   if (confirm("Are you sure you want to delete this message?")) {
+    //     // this is going to change in 2.0, so I may need to rewrite this using
+    //     // $index and splice
+    //     this.messages.$remove(message);
+    //   }
+    // }
   }
 });
