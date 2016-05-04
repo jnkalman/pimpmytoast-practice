6cd447b3ec492cd60b5bb5a43eb236eb9bee55de
Jake Kalman <jacob.kalman@fmr.com>
2016-05-04 12:14:03 -0400

    added firebase support, added firebase hosting for now, added dynamic loading and saving

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ var Firebase = require('firebase');
 Vue.use(require('vuefire'));
 Vue.use(require('vue-resource'));
 
+
 /* EVENTS VUE */
 
 new Vue({
@@ -13,70 +14,47 @@ new Vue({
   el: '#events',
   // set up firebase connection
   firebase: {
-    dataRef: new Firebase('https://radiant-torch-6650.firebaseio.com/'),
-
-    eventData: {
-      source: new Firebase('https://radiant-torch-6650.firebaseio.com/'),
-      asObject: true
-    }
+    events: new Firebase('https://radiant-torch-6650.firebaseio.com/events')
   },
-  // register any values or collections that hold data
-  // for the application - where viewmodel data is registered
-  data: {
-    event: {name: '', description: '', date: ''},
-    events: []
-  },
-
   // anything within the ready function will run when
   // the application loads, call methods to initialize the app
   // with data
   ready: function() {
     // when the application loads, call the initialize data method
-    this.fetchEvents();
-
+    //this.fetchEvents();
+    // var events = this.$firebaseRefs.eventsDataRef;
+    this.subscribe();
     //this.$set('events', eventData);
+  },
+
+  // initial object
+  data: {
+    event: { name: '', description: '', date: '' }
   },
 
   // custom methods registered here
   methods: {
-
+    subscribe: function() {
+      // var events = this.events;
+      //
+      // events.on("value", function(snapshot) {
+      //   console.log(snapshot.val());
+      // }, function (errorObject) {
+      //   console.log("The read failed: " + errorObject.code);
+      // });
+    },
     // method to retrieve and set data
     fetchEvents: function() {
-      var events = this.$firebaseRefs.dataRef;
-      // $set is a convenience method provided by Vue that is similar to pushing
-      // data onto an array
-      //
-      // this.$get('events', function() {
-      //   try {
-      //
-      //   } catch(err) {}
-      // });
-      this.$firebaseRefs.eventData.on("value", function(snapshot) {
-        console.log(snapshot.val());
-      }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-      });
 
-
-      // ^^ Temp code until JSON get/parse \/ works
-
-      /*$.getJSON('data.json');
-      try {
-        $.getJSON
-      }*/
-      // this.$http.get('/data.json').then(function(events) {
-      //   this.$set('events', events);
-      // }).catch(function(error) {
-      //   console.log(error);
-      // });
 
     },
 
     // adds an event to the existing events array
     addEvent: function() {
       if (this.event.name) {
+        var events = this.$firebaseRefs.events;
         // add event
-        this.events.push(this.event);
+        events.push(this.event);
         // reset event
         this.event = {name: '', description: '', date: ''};
       }
